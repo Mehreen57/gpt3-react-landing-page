1099cf5f9aac6c1017db2604cac50b6db5d18e2e
mehreen57 <mehreenbhat77@gmail.com>
2022-03-02 16:50:11 +0530

    added sections

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// Importing all the components here
+export { default as Article } from './article/Article';
+export { default as Brand } from './brand/Brand';
+export { default as Cta } from './cta/Cta';
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './blog.css';
+
+const Blog = () => {
+    return (
+        <div className=''>
+            
+        </div>
+    )
+}
+
+export default Blog
